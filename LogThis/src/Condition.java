786c16d860cc7a18c4921c8346d15f76c04164ce
kazschuri <github@ldka.de>
2014-02-19 18:51:01 +0100

    moved contains Element to a general Class

--- LogThis/src/Condition.java
+++ LogThis/src/Condition.java
@@ -28,30 +28,7 @@ public class Condition {
 		super();
 		
 	}
-	/**
-	 * finding an element in an array
-	 * 
-	 * @param haystack the haystack to search in
-	 * @param needle the needle to search for
-	 * 
-	 * @return true if found
-	 */
-	public static boolean containsElement(String[] haystack, String needle) {
-		
-		boolean found = false;
-		
-		for (int i = 0; i < haystack.length; i++) {
-			
-			if (haystack[i].contains(needle)) {
-				
-				found = true;
-				break;
-				
-			}
-		}
-		
-		return found;
-	}
+
 	
 	/**
 	 * selects the category and the subcategory as haystack and searches for the element
@@ -86,7 +63,7 @@ public class Condition {
 				
 			}
 				
-				result = containsElement(haystack, element);	// look in appropriate subcategory for element
+				result = GeneralMethods.containsElement(haystack, element);	// look in appropriate subcategory for element
 			
 		}
 		if (this.category.equals("Timestamp")) { 		
